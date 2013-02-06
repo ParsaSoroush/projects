9ddc2b8c9a990628be0cef7e5395b4d58986cf03
parsa sorouash <parsakingkp@gmail.com>
2013-02-06 07:22:39 +0330

    Below is the function display_student(name, age). Assign a new name show_tudent(name, age) to it and call it using the new name.

--- 23_Create_an_outer_function.py
+++ 23_Create_an_outer_function.py
@@ -1,14 +1,11 @@
-def outer_fun(a, b):
+def sum_num(a, b):
     square = a ** 2
 
-    # inner function
     def addition(a, b):
         return a + b
 
-    # call inner function from outer function
     add = addition(a, b)
-    # add 5 to the result
     return add + 5
 
-result = outer_fun(5, 10)
+result = sum_num(5, 10)
 print(result)
