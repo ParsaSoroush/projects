fa9497152b48bc14e14c4d9a40500b9cac88ae12
parsa sorouash <parsakingkp@gmail.com>
2013-02-06 17:30:27 +0330

    Print the value of key ‘history’ from the below dict sampleDict = {     class: {         student: {             name: Mike,             marks: {                 physics: 70,                 history: 80             }         }     } } Expected output: 80

---
+++ 42_Print_the_value.py
@@ -0,0 +1,13 @@
+dict1 = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+
+print(dict1['class']['student']['marks']['history'])
