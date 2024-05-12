be0fddfc0e0ed37e7a5da963063ba0ac0520d346
parsa sorouash <parsakingkp@gmail.com>
2024-05-12 19:07:11 +0430

    Create an outer function that will accept two parameters, a and b Create an inner function inside an outer function that will calculate the addition of a and b At last, an outer function will add 5 into addition and return it

--- 22_Create_a_function_with_a_default_argument.py
+++ 22_Create_a_function_with_a_default_argument.py
@@ -1,5 +1,5 @@
-def show_salary(name, salary=9000):
+def show_employee(name, salary=9000):
     print("name:", name, "salary", salary)
 
-show_salary("ben", 1200)
-show_salary("jessa")
+show_employee("ben", 1200)
+show_employee("jessa")
