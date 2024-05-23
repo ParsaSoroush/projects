40d49fc83ee30fa24ad06c5730b3ef3429444a8a
parsa sorouash <parsakingkp@gmail.com>
2024-05-23 14:39:33 +0430

    give two number and one operator to calculate

--- 36_new_calculation.py
+++ 36_new_calculation.py
@@ -1,4 +1,4 @@
-input_string = input("Enter two numbers and an operator (e.g., 5 + 3): ")
+input_string = input("Enter two numbers and an operator (for exampel: 5 + 10: ")
 
 input_list = input_string.split()
 num1 = float(input_list[0])
@@ -13,7 +13,7 @@ elif operator == '-':
 elif operator == '*':
     result = num1 * num2
 elif operator == '/':
-    if num2 != 0:  # Check for division by zero
+    if num2 != 0:
         result = num1 / num2
     else:
         print("Error: Division by zero!")
