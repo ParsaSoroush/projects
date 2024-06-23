aa2a59ae36efb84fc2bfa87602c5f435a8b8f378
parsa sorouash <parsakingkp@gmail.com>
2024-06-24 03:20:46 +0430

    On the occasion of Nowruz, three old friends want to meet each other. Azarmehr, Azargun and Mehrayin plan to meet at the same place. The house of these three people is located on the right line (x-axis), Azarmehr's house is located at point x1, Azargun's house is located at point x2, and Mehraain's house is located at point x3

--- 84_is_it_palindrom.py
+++ 84_is_it_palindrom.py
@@ -1,5 +1,5 @@
 def is_palindrome(word):
-    word = word.lower()
+    word = word.lower()  
     return word == word[::-1]
 
 
---
+++ 86_nowroz_meeting.py
@@ -0,0 +1,6 @@
+def shortest_distance(x1, x2, x3):
+    return abs(x3)
+
+
+print(shortest_distance(6, 9, 10))
+print(shortest_distance(6, 9, 10))
