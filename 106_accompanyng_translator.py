6a1cdf881c652887ef89b6b36f1bc9a99517e623
PARSAKING77 <parsasoroush13@gmail.com>
2024-08-24 16:27:15 +0330

    this code provides a basic framework for translating sentences from various languages to Persian, but it has limitations and may require additional development to handle more complex translation tasks.

--- 106_accompanyng_translator.py
+++ 106_accompanyng_translator.py
@@ -1,33 +1,52 @@
 n = int(input())
 
-
 dictionary = {}
-
 
 for _ in range(n):
     words = input().split()
-    english, french, german, persian = words[1], words[2], words[3], words[0]
+    persian, english, french, german = words[0], words[1], words[2], words[3]
     dictionary[english] = persian
     dictionary[french] = persian
     dictionary[german] = persian
 
 sentence = input().split()
 
+language_map = {
+    'i': 'english',
+    'me': 'english',
+    'my': 'english',
+    'je': 'french',
+    'moi': 'french',
+    'mon': 'french',
+    'ich': 'german',
+    'mich': 'german',
+    'mein': 'german',
+    'yo': 'spanish',
+    'me': 'spanish',
+    'mi': 'spanish',
+    'io': 'italian',
+    'me': 'italian',
+    'mio': 'italian',
+    'eu': 'portuguese',
+    'me': 'portuguese',
+    'meu': 'portuguese',
+    'ja': 'dutch',
+    'mij': 'dutch',
+    'mijn': 'dutch',
+    'man': 'persian',  # added persian language
+    # add more languages here...
+}
 
-if sentence[0].lower() == 'i':
-    language = 'english'
-elif sentence[0].lower() == 'je':
-    language = 'french'
-elif sentence[0].lower() == 'ich':
-    language = 'german'
+language = language_map.get(sentence[0].lower())
+
+if language is None:
+    print("Unknown language")
+    exit()  # or raise an exception
 
 translated_sentence = []
 for word in sentence:
-    if language == 'english':
-        if word in dictionary:
-            translated_sentence.append(dictionary[word])
-        else:
-            translated_sentence.append(word)
+    if word in dictionary:
+        translated_sentence.append(dictionary[word])
     else:
         translated_sentence.append(word)
 
