d9756274333f7c9a1a83566c0797b83670025e7b
PARSAKING77 <parsasoroush13@gmail.com>
2024-08-25 16:44:44 +0330

    The code is designed to solve a problem where you need to compare the average physical characteristics of two classrooms of students.

---
+++ 107_health_aplication.py
@@ -0,0 +1,67 @@
+class Student:
+    def __init__(self, age, height, weight):
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+class Classroom:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+
+    def add_student(self, age, height, weight):
+        self.students.append(Student(age, height, weight))
+
+    def calculate_average(self):
+        total_age = sum(student.age for student in self.students)
+        total_height = sum(student.height for student in self.students)
+        total_weight = sum(student.weight for student in self.students)
+        average_age = total_age / len(self.students)
+        average_height = total_height / len(self.students)
+        average_weight = total_weight / len(self.students)
+        return average_age, average_height, average_weight
+
+def main():
+    classroom_a = Classroom('A')
+    classroom_b = Classroom('B')
+
+    num_students_a = int(input())
+    ages_a = list(map(int, input().split()))
+    heights_a = list(map(int, input().split()))
+    weights_a = list(map(int, input().split()))
+
+    num_students_b = int(input())
+    ages_b = list(map(int, input().split()))
+    heights_b = list(map(int, input().split()))
+    weights_b = list(map(int, input().split()))
+
+    for i in range(num_students_a):
+        classroom_a.add_student(ages_a[i], heights_a[i], weights_a[i])
+
+    for i in range(num_students_b):
+        classroom_b.add_student(ages_b[i], heights_b[i], weights_b[i])
+
+    avg_age_a, avg_height_a, avg_weight_a = classroom_a.calculate_average()
+    avg_age_b, avg_height_b, avg_weight_b = classroom_b.calculate_average()
+
+    print(f"{avg_age_a:.1f}")
+    print(f"{avg_height_a:.1f}")
+    print(f"{avg_weight_a:.1f}")
+    print(f"{avg_age_b:.1f}")
+    print(f"{avg_height_b:.1f}")
+    print(f"{avg_weight_b:.1f}")
+
+    if avg_height_a > avg_height_b:
+        print("A")
+    elif avg_height_a < avg_height_b:
+        print("B")
+    else:
+        if avg_weight_a < avg_weight_b:
+            print("A")
+        elif avg_weight_a > avg_weight_b:
+            print("B")
+        else:
+            print("Same")
+
+if __name__ == "__main__":
+    main()
