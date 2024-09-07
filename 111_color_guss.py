8085f745cd2e9391917e9d5bd6bed6cdc4578de0
PARSAKING77 <parsasoroush13@gmail.com>
2024-09-07 08:13:36 +0330

    This code is a simple color guessing game. It imports the random module to select a random color from a list of 15 colors. The color_guess function chooses a random color and asks the user to guess it. The user's guess is compared to the chosen color, and if it's incorrect, the user is asked to try again. This continues until the user guesses the correct color, at which point a congratulatory message is printed. The game continues to prompt the user for input until the correct color is guessed. The color_guess function is then called to start the game.

---
+++ 111_color_guss.py
@@ -0,0 +1,14 @@
+import random
+
+
+def color_guess():
+    colors = ["red", "blue", "green", "yellow", "purple", "orange", "black", "white","brown", "pink", "gray", "silver","gold","crimson","bronze"]
+    color_to_guess = random.choice(colors)
+    guess = None
+    while guess != color_to_guess:
+        guess = input("pick your color: ")
+        if guess != color_to_guess:
+            print("your color is incorrect. try again.: ")
+    print("WWWWWWWWWWWWOOOOOOOOOOOOOOOOOOOOOOOOOOWWWWWWWWWWWWWWWWWWWWW!!!!!!!!!!!!!!!!!!!!!! YOUR COLOR IS CORRECT!!!!!!!!!!!!!!!!!!")
+
+color_guess()
