5f17deb4b26ea99449494b9224bcd1fc98c84ac0
PARSAKING77 <parsakingkp@gmail.com>
2024-09-24 12:58:19 +0330

    the secend update of makeing basic instagram

--- 126_make_basic_instagram.py
+++ 126_make_basic_instagram.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Depends
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, session
 from sqlalchemy import Column, Integer, String, Index
+from pydantic import BaseModel
+
 
 app = FastAPI()
 engine = create_engine("sqlite:///Instagram.db", connect_args={'check_same_thread':False})
 Base = declarative_base()
+router = APIRouter(prefix="/user", tags=['user'])
+
+
 
 class UserBase(Base):
     __tablename__ = "user"
@@ -20,9 +25,7 @@ class UserBase(Base):
 
 Base.metadata.create_all(engine)
 
-@app.get('/')
-def home():
-    return "First Page"
+
 
 sessionlocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
@@ -32,3 +35,34 @@ def get_db():
         yield session
     finally:
         session.close()
+
+class User(BaseModel):
+    username:str
+    email:str
+    password:str
+
+
+class UserDisplay(BaseModel):
+    username:str
+    email:str
+    class Config:
+        orm_mode=True
+def create_user(request: User, db: session):
+    user = User(
+        username=request.username,
+        password=request.password, #hash
+        email=request.email)
+    
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+@router.post("/", response_model=UserDisplay)
+def create_user(request: User, db: session = Depends(get_db)):
+    return create_user(request, db)
+
+
+@app.get('/')
+def home():
+    return "First Page"
