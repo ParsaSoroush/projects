ed915c1e493d5a3bc9205ed32aba5fd6cd10ce58
PARSAKING77 <parsakingkp@gmail.com>
2024-09-24 12:31:35 +0330

    first update of makeing a basic version of instagram

--- 126_make_basic_instagram.py
+++ 126_make_basic_instagram.py
@@ -1,11 +1,28 @@
+from fastapi import FastAPI
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Index
 
-
+app = FastAPI()
 engine = create_engine("sqlite:///Instagram.db", connect_args={'check_same_thread':False})
 Base = declarative_base()
 
+class UserBase(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    email = Column(String)
+
+    __table_args__ = (Index('id_index', 'id'),) 
+
+Base.metadata.create_all(engine)
+
+@app.get('/')
+def home():
+    return "First Page"
 
 sessionlocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
@@ -13,6 +30,5 @@ def get_db():
     session = sessionlocal()
     try:
         yield session
-
     finally:
         session.close()
